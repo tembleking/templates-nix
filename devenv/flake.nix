a183de87f71931a07fba99a745f400d0875597cd
Fede Barcelona <fede_rico_94@hotmail.com>
2024-05-30 09:16:34 +0200

    feat: add devenv template

---
+++ devenv/flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+        with pkgs; {
+          devShell = mkShell {
+            buildInputs = [
+              # Add here dependencies for the project.
+            ];
+          };
+
+          formatter = alejandra;
+        }
+    );
+}
---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A collection of flake templates";
+
+  outputs = { self }: {
+
+    templates = {
+
+      devenv = {
+        path = ./devenv;
+        description = "A very basic flake for development";
+      };
+
+    };
+
+    defaultTemplate = self.templates.trivial;
+
+  };
+}
