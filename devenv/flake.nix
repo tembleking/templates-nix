9c8c9a1e41cdc3d97e7f5a5ed7879b7b5cca4977
Fede Barcelona <fede_rico_94@hotmail.com>
2024-11-29 11:06:36 +0100

    feat(devenv): add package building and overlay publishing

--- devenv/flake.nix
+++ devenv/flake.nix
@@ -9,24 +9,35 @@
       nixpkgs,
       flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      in
-      {
-        devShells.default =
-          with pkgs;
-          mkShell {
-            packages = [
-              # Add here dependencies for the project.
-            ];
+    let
+      overlays.default = final: prev: {
+        # packageName = prev.callPackage ./package.nix {};
+      };
+      flake = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [ self.overlays.default ];
           };
+        in
+        {
+          packages = with pkgs; {
+            inherit packageName;
+            default = packageName;
+          };
+          devShells.default =
+            with pkgs;
+            mkShell {
+              packages = [
+                # Add here dependencies for the project.
+              ];
+            };
 
-        formatter = pkgs.nixfmt-rfc-style;
-      }
-    );
+          formatter = pkgs.nixfmt-rfc-style;
+        }
+      );
+    in
+    flake // { inherit overlays; };
 }
