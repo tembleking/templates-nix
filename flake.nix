6d1b24ec5f6fc7c208ef12cac4dd0d6a51c235b6
Fede Barcelona <fede_rico_94@hotmail.com>
2025-01-27 14:34:43 +0100

    chore: add both python-uv-app and python-uv-lib

--- flake.nix
+++ flake.nix
@@ -1,24 +1,36 @@
 {
   description = "A collection of flake templates";
 
-  outputs = {self}: {
-    templates = {
-      devenv = {
-        path = ./devenv;
-        description = "A very basic flake for development";
+  outputs =
+    { self }:
+    {
+      templates = {
+        devenv = {
+          path = ./devenv;
+          description = "A very basic flake for development";
+        };
+
+        python-poetry = {
+          path = ./python-poetry;
+          description = "A basic template for Python applications managed with poetry";
+        };
+
+        gcp-vm-image = {
+          path = ./gcp-vm-image;
+          description = "NixOS configuration for GCP VM Image creation";
+        };
+
+        python-uv-app = {
+          path = ./python-uv-app;
+          description = "Python UV Application";
+        };
+
+        python-uv-lib = {
+          path = ./python-uv-lib;
+          description = "Python UV Library";
+        };
       };
 
-      python-poetry = {
-        path = ./python-poetry;
-        description = "A basic template for Python applications managed with poetry";
-      };
-
-      gcp-vm-image = {
-        path = ./gcp-vm-image;
-        description = "NixOS configuration for GCP VM Image creation";
-      };
+      defaultTemplate = self.templates.devenv;
     };
-
-    defaultTemplate = self.templates.devenv;
-  };
 }
